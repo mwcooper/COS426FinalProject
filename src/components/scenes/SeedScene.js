32faff8c6bea3469a3968b95b7e7492ad71c9828
mwcooper <70532548+mwcooper@users.noreply.github.com>
2022-04-22 11:08:43 -0400

    some stuff

--- src/components/scenes/SeedScene.js
+++ src/components/scenes/SeedScene.js
@@ -16,7 +16,7 @@ class SeedScene extends Scene {
         };
 
         // Set background to a nice color
-        this.background = new Color(0x7ec0ee);
+        this.background = new Color(0x050018);
 
         // Add meshes to scene
         const land = new Land();
